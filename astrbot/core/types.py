977f0150a5dcc57c7eaec56e91fc3fae994dfc5d
LIghtJUNction <lightjunction.me@gmail.com>
2025-04-19 20:22:57 +0800

    Delete astrbot/core/types.py
    撤销

--- astrbot/core/types.py
+++
@@ -1,4 +0,0 @@
-from typing import TypeAlias
-
-ConfigValue: TypeAlias = str | int | float | bool | list[str | int | float | bool] | dict[str, "ConfigValue"] | None
-JsonValue: TypeAlias = str | int | float | bool | list["JsonValue"] | dict[str, "JsonValue"] | None
