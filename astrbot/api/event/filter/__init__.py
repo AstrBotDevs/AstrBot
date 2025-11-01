e8d294f4ec1698e55708b4efb17ac0635ee953f7
Soulter <905617992@qq.com>
2025-11-01 12:40:18 +0800

    style(__init__.py): 优化导入语句格式，减少冗余行

--- astrbot/api/event/filter/__init__.py
+++ astrbot/api/event/filter/__init__.py
@@ -8,48 +8,24 @@ from astrbot.core.star.filter.platform_adapter_type import (
     PlatformAdapterType,
     PlatformAdapterTypeFilter,
 )
-from astrbot.core.star.register import (
-    register_after_message_sent as after_message_sent,
-)
-from astrbot.core.star.register import (
-    register_command as command,
-)
-from astrbot.core.star.register import (
-    register_command_group as command_group,
-)
-from astrbot.core.star.register import (
-    register_custom_filter as custom_filter,
-)
-from astrbot.core.star.register import (
-    register_event_message_type as event_message_type,
-)
-from astrbot.core.star.register import (
-    register_llm_tool as llm_tool,
-)
-from astrbot.core.star.register import (
-    register_on_astrbot_loaded as on_astrbot_loaded,
-)
+from astrbot.core.star.register import register_after_message_sent as after_message_sent
+from astrbot.core.star.register import register_command as command
+from astrbot.core.star.register import register_command_group as command_group
+from astrbot.core.star.register import register_custom_filter as custom_filter
+from astrbot.core.star.register import register_event_message_type as event_message_type
+from astrbot.core.star.register import register_llm_tool as llm_tool
+from astrbot.core.star.register import register_on_astrbot_loaded as on_astrbot_loaded
 from astrbot.core.star.register import (
     register_on_decorating_result as on_decorating_result,
 )
-from astrbot.core.star.register import (
-    register_on_llm_request as on_llm_request,
-)
-from astrbot.core.star.register import (
-    register_on_llm_response as on_llm_response,
-)
-from astrbot.core.star.register import (
-    register_on_platform_loaded as on_platform_loaded,
-)
-from astrbot.core.star.register import (
-    register_permission_type as permission_type,
-)
+from astrbot.core.star.register import register_on_llm_request as on_llm_request
+from astrbot.core.star.register import register_on_llm_response as on_llm_response
+from astrbot.core.star.register import register_on_platform_loaded as on_platform_loaded
+from astrbot.core.star.register import register_permission_type as permission_type
 from astrbot.core.star.register import (
     register_platform_adapter_type as platform_adapter_type,
 )
-from astrbot.core.star.register import (
-    register_regex as regex,
-)
+from astrbot.core.star.register import register_regex as regex
 
 __all__ = [
     "CustomFilter",
