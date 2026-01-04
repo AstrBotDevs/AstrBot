ded10e2b6587a0cf2de3bffed9a5712a88cc1976
advent259141 <2968474907@qq.com>
2026-01-04 15:06:29 +0800

    ruff check

--- astrbot/core/star/register/star_handler.py
+++ astrbot/core/star/register/star_handler.py
@@ -355,7 +355,9 @@ def register_on_waiting_llm_request(**kwargs):
     """
 
     def decorator(awaitable):
-        _ = get_handler_or_create(awaitable, EventType.OnWaitingLLMRequestEvent, **kwargs)
+        _ = get_handler_or_create(
+            awaitable, EventType.OnWaitingLLMRequestEvent, **kwargs
+        )
         return awaitable
 
     return decorator
