4282f4f8a55471a190b614f4a7b6419ee804c4ae
whatevertogo <whatevertogo@users.noreply.github.com>
2026-03-14 17:41:12 +0800

    delete old sdk

--- src/astrbot_sdk/api/event/__init__.py
+++
@@ -1,5 +0,0 @@
-from .astr_message_event import AstrMessageEvent
-
-__all__ = [
-    "AstrMessageEvent",
-]
--- src/astrbot_sdk/runtime/rpc/server/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base import JSONRPCServer
-from .stdio import StdioServer
-from .websockets import WebSocketServer
-
-__all__ = [
-    "JSONRPCServer",
-    "StdioServer",
-    "WebSocketServer",
-]
