6b97a0051ebe5fa6fb2a6d018f22b623db596f5d
Lishiling <m18384519631@163.com>
2026-03-25 09:55:52 +0800

    add(provider):添加五大提供商和其对应的注册器到astrbot.api

--- astrbot/api/provider/__init__.py
+++ astrbot/api/provider/__init__.py
@@ -1,5 +1,6 @@
 from astrbot.core.db.po import Personality
-from astrbot.core.provider import Provider, STTProvider
+from astrbot.core.provider import Provider, STTProvider, STTProvider, EmbeddingProvider, RerankProvider
+from astrbot.core.provider.register import register_provider_adapter
 from astrbot.core.provider.entities import (
     LLMResponse,
     ProviderMetaData,
@@ -15,4 +16,8 @@ __all__ = [
     "ProviderRequest",
     "ProviderType",
     "STTProvider",
+    "STTProvider",
+    "EmbeddingProvider",
+    "RerankProvider",
+    "register_provider_adapter"
 ]
--- astrbot/core/provider/__init__.py
+++ astrbot/core/provider/__init__.py
@@ -1,4 +1,4 @@
 from .entities import ProviderMetaData
-from .provider import Provider, STTProvider
+from .provider import Provider, STTProvider, STTProvider, EmbeddingProvider, RerankProvider
 
-__all__ = ["Provider", "ProviderMetaData", "STTProvider"]
+__all__ = ["Provider", "ProviderMetaData", "STTProvider", "STTProvider", "EmbeddingProvider", "RerankProvider"]
