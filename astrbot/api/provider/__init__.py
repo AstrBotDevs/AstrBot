ca162d02cae9af861d1f2092d31e9b879a705804
Lishiling <m18384519631@163.com>
2026-03-25 10:40:09 +0800

    ruff:进行ruff格式整理

--- astrbot/api/provider/__init__.py
+++ astrbot/api/provider/__init__.py
@@ -1,5 +1,11 @@
 from astrbot.core.db.po import Personality
-from astrbot.core.provider import Provider, STTProvider, STTProvider, EmbeddingProvider, RerankProvider
+from astrbot.core.provider import (
+    Provider,
+    STTProvider,
+    STTProvider,
+    EmbeddingProvider,
+    RerankProvider,
+)
 from astrbot.core.provider.register import register_provider_adapter
 from astrbot.core.provider.entities import (
     LLMResponse,
@@ -19,5 +25,5 @@ __all__ = [
     "STTProvider",
     "EmbeddingProvider",
     "RerankProvider",
-    "register_provider_adapter"
+    "register_provider_adapter",
 ]
--- astrbot/core/provider/__init__.py
+++ astrbot/core/provider/__init__.py
@@ -1,4 +1,17 @@
 from .entities import ProviderMetaData
-from .provider import Provider, STTProvider, STTProvider, EmbeddingProvider, RerankProvider
+from .provider import (
+    Provider,
+    STTProvider,
+    STTProvider,
+    EmbeddingProvider,
+    RerankProvider,
+)
 
-__all__ = ["Provider", "ProviderMetaData", "STTProvider", "STTProvider", "EmbeddingProvider", "RerankProvider"]
+__all__ = [
+    "Provider",
+    "ProviderMetaData",
+    "STTProvider",
+    "STTProvider",
+    "EmbeddingProvider",
+    "RerankProvider",
+]
