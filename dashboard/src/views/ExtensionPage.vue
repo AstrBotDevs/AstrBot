9ca22c5efd928084faca01e7b9a8e19de1ab9e72
冷石Boy <coldstoneboy@qq.com>
2026-03-26 12:01:21 +0800

    docs(ExtensionPage): 更新插件文档链接地址
    将插件文档链接地址更新为新的官方文档地址

--- dashboard/src/views/ExtensionPage.vue
+++ dashboard/src/views/ExtensionPage.vue
@@ -227,7 +227,7 @@ const {
         <v-btn
           variant="text"
           prepend-icon="mdi-book-open-variant"
-          href="https://astrbot.app/dev/plugin.html"
+          href="https://docs.astrbot.app/dev/star/plugin-new.html"
           target="_blank"
           color="primary"
           class="text-none"
