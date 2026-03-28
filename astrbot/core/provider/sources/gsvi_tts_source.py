d7b0a72497f489ab92d4b77b9662cd1c6fec90f7
Rain-0x01-39 <83620631+Rain-0x01-39@users.noreply.github.com>
2026-03-28 17:18:53 +0800

    fix(gsvi_tts): Adjust wherever the Authorization header is built to only include it when `self.api_key` is truthy Delete some comments

--- astrbot/core/provider/sources/gsvi_tts_source.py
+++ astrbot/core/provider/sources/gsvi_tts_source.py
@@ -9,42 +9,6 @@ from ..entities import ProviderType
 from ..provider import TTSProvider
 from ..register import register_provider_adapter
 
-# 贴个作为参考
-# curl -X 'POST' \
-#   'https://gsv2p.acgnai.top/infer_single' \
-#   -H 'accept: application/json' \
-#   -H 'Authorization: Bearer 2e3d9b7*************************' \
-#   -H 'Content-Type: application/json' \
-#   -d '{
-#   "version": "v4",
-#   "model_name": "原神-中文-芙宁娜_ZH",
-#   "prompt_text_lang": "中文",
-#   "emotion": "默认",
-#   "text": "还是面向人工智能编程的",
-#   "text_lang": "中文",
-#   "top_k": 10,
-#   "top_p": 1,
-#   "temperature": 1,
-#   "text_split_method": "按标点符号切",
-#   "batch_size": 1,
-#   "batch_threshold": 0.75,
-#   "split_bucket": true,
-#   "speed_facter": 1,
-#   "fragment_interval": 0.3,
-#   "media_type": "wav",
-#   "parallel_infer": true,
-#   "repetition_penalty": 1.35,
-#   "seed": -1,
-#   "sample_steps": 16,
-#   "if_sr": false
-# }'
-
-# {
-#   "msg": "合成成功",
-#   "audio_url": "https://gsv2p.acgnai.top/outputs/9264dd20655fa31660376801639c9a73.wav"
-# }
-
-# {"msg":"参数错误","audio_url":""}
 
 @register_provider_adapter(
     "gsvi_tts_api",
@@ -72,6 +36,10 @@ class ProviderGSVITTS(TTSProvider):
         path = Path(temp_dir) / f"gsvi_tts_{uuid.uuid4()}.wav"
         url = f"{self.api_base}/infer_single"
 
+        headers = {"Content-Type": "application/json"}
+        if self.api_key:
+            headers["Authorization"] = f"Bearer {self.api_key}"
+
         data = {
             "dl_url": self.api_base,
             "version": self.version,
@@ -84,7 +52,7 @@ class ProviderGSVITTS(TTSProvider):
 
         async with aiohttp.ClientSession() as session:
             async with session.post(
-                url, json=data, headers={"Authorization": f"Bearer {self.api_key}"}
+                url, json=data, headers=headers
             ) as response:
                 if response.status == 200:
                     resp_json = await response.json()
