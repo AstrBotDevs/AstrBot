b187ac709cc6d5bfae43457e10c361a580b1632f
shuiping233 <1944680304@qq.com>
2026-04-17 19:38:15 +0800

    unittest: 部分重构test_kook_event.py和test_kook_types.py 单测

--- tests/test_kook/test_kook_types.py
+++ tests/test_kook/test_kook_types.py
@@ -24,7 +24,7 @@ from astrbot.core.platform.sources.kook.kook_types import (
     SectionModule,
     KookCardMessageContainer,
 )
-from tests.test_kook.shared import TEST_DATA_DIR
+from tests.test_kook.shared import TEST_DATA_DIR, KookEventDataPath
 
 
 def test_kook_card_message_container_append():
@@ -109,29 +109,30 @@ def test_all_kook_card_type():
     ).to_json(indent=4, ensure_ascii=False)
     assert json_output == expect_json_data
 
+
 @pytest.mark.parametrize(
-    "expected_json_data_filename",
+    "expected_json_data_path",
     [
-        ("kook_ws_event_group_message.json"),
-        ("kook_ws_event_hello.json"),
-        ("kook_ws_event_message_with_card_1.json"),
-        ("kook_ws_event_message_with_card_2.json"),
-        ("kook_ws_event_ping.json"),
-        ("kook_ws_event_pong.json"),
-        ("kook_ws_event_private_message.json"),
-        ("kook_ws_event_private_system_message.json"),
-        ("kook_ws_event_reconnect_err.json"),
-        ("kook_ws_event_resume_ack.json"),
-        ("kook_ws_event_resume.json"),
-        
+        (KookEventDataPath.GROUP_MESSAGE_WITH_MENTION),
+        (KookEventDataPath.GROUP_MESSAGE),
+        (KookEventDataPath.HELLO),
+        (KookEventDataPath.MESSAGE_WITH_CARD_1),
+        (KookEventDataPath.MESSAGE_WITH_CARD_2),
+        (KookEventDataPath.PING),
+        (KookEventDataPath.PONG),
+        (KookEventDataPath.PRIVATE_MESSAGE),
+        (KookEventDataPath.PRIVATE_SYSTEM_MESSAGE),
+        (KookEventDataPath.RECONNECT_ERR),
+        (KookEventDataPath.RESUME_ACK),
+        (KookEventDataPath.RESUME),
     ],
 )
-def test_websocket_event_type_parse(expected_json_data_filename:str):
-    expected_json_data_str =(TEST_DATA_DIR / expected_json_data_filename).read_text(encoding="utf-8")
+def test_websocket_event_type_parse(expected_json_data_path: Path):
+    expected_json_data_str = (expected_json_data_path).read_text(encoding="utf-8")
     event = KookWebsocketEvent.from_json(
         expected_json_data_str,
     )
-    event_dict = event.to_dict(mode="json",exclude_unset=True,exclude_none=False)
+    event_dict = event.to_dict()
     assert event_dict == json.loads(expected_json_data_str)
 
 
@@ -141,8 +142,7 @@ def test_websocket_event_create():
         data=None,
         sn=0,
     )
-    assert ping_data.to_dict(mode="json")== {
+    assert ping_data.to_dict(exclude_none=True, exclude_unset=False) == {
         "s": KookMessageSignal.PING.value,
         "sn": 0,
     }
-    
