e213e59611dd13c9212309a9f420847941a779d0
alma <xayozijopi960@gmail.com>
2026-09-23 00:17:38 +0800

    fix(dashboard): restore the d3 import used by long-term memory

--- dashboard/src/views/alkaid/LongTermMemory.vue
+++ dashboard/src/views/alkaid/LongTermMemory.vue
@@ -254,7 +254,7 @@
 
 <script>
 import { pluginExtensionApi } from '@/api/v1';
-// import * as d3 from "d3"; // npm install d3
+import * as d3 from "d3";
 import { useModuleI18n } from '@/i18n/composables';
 import { normalizeTextInput } from '@/utils/inputValue';
 
